0fe8e1224d88cea62c8a2b11125da8cd6f85aafc
Anastasia <atamantseva2016@ya.ru>
2019-09-02 21:28:44 +0300

    correction2

--- 1.2-strings-and-forms/1.2.1-registration/index.php
+++ 1.2-strings-and-forms/1.2.1-registration/index.php
@@ -10,20 +10,19 @@ $code = $_POST['code'];
 $codeWord = 'netology';
 
 
-if(!preg_match("/^[0-9a-zA-Z\._\- ]+$/", $login)){
+if(!preg_match('/[^@\/\*?,;:]/', $login)){
 	echo 'Поле имени содержит недопустимые символы @/*?,;:.<br>';
 }elseif (strlen($password) < 8) {
 	echo 'Пароль должен быть не менее 8 символов <br>';
 }elseif (!preg_match('/[0-9a-z]+@[a-z]+.[a-z]/', $email)){
 	echo 'Введите почтовый адрес в формате somemail@email.com <br>';
-}elseif (strlen($firstName) < 1||strlen($lastName) <1 ||strlen($firstName) <1 ){
+}elseif (strlen($firstName) < 1||strlen($lastName) < 1 || strlen($firstName) < 1 ){
 	echo 'Длина каждого из полей должна быть больше 1-го символа <br>';
 }elseif ($codeWord !=trim(strtolower($code))) {
 	echo 'Кодовое слово указано неверно <br>';
 } else{
 	echo 'Регистрация прошла успешно <br>';
 }
-
 
  
 ?>
