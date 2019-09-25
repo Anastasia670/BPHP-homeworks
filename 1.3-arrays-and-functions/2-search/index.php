99c7b7daf056cb74f2b1acfaec1911dd6a83601b
Anastasia <atamantseva2016@ya.ru>
2019-09-25 21:08:32 +0300

    correction

--- 1.3-arrays-and-functions/2-search/index.php
+++ 1.3-arrays-and-functions/2-search/index.php
@@ -1,103 +1,87 @@
-<pre>
+
 
 <?php
 
-function generate($rows, $placesPerRow, $avaliableCount) {
-	$map = [];
 
-    if ($avaliableCount < ($rows * $placesPerRow)) {
-        return false;
-    }
-    
-    
+$map = [
 
+    [FALSE,TRUE,FALSE,FALSE,FALSE,FALSE],
 
-    for ($i=0; $i < $rows; $i++) { 
-        for ($j=0; $j < $placesPerRow; $j++) {             
+    [FALSE,FALSE,FALSE,FALSE,FALSE,FALSE],
 
-            $map[$i][$j] = false;
-            
-        };
-    };
+    [FALSE,FALSE,FALSE,FALSE,FALSE,FALSE],
 
-    return $map;
-};
+    [FALSE,FALSE,FALSE,FALSE,FALSE,FALSE],
 
+    [FALSE,FALSE,FALSE,FALSE,FALSE,FALSE],
 
-function reserve(&$map, $row, $place) {
+    [FALSE,FALSE,FALSE,FALSE,FALSE,FALSE],
 
-    if ($map[$row-1][$place-1] == false) {
-        $map[$row-1][$place-1] = true;
-       
-        return true;
-    } else {
-        return false;
-    };
-};
-
-
-$availChairs = 50;
-$map = generate(5, 8, $availChairs);
-$requiredRow = 3;
-$requiredPlace = 5;
-    
-
-
-$reverve = reserve($map, $requiredRow, $requiredPlace);
-
-// logReserve($requiredRow, $requiredPlace, $reverve);
-
-
-
-// function logReserve($row, $place, $result){
-//     if ($result) {
-//         echo "Ваше место забронировано! Ряд $row, место $place".PHP_EOL;
-//     } else {
-//         echo "Что-то пошло не так=( Бронь не удалась".PHP_EOL;
-
-//     };
-
-// };
-
-
+];
 
 function groupReserve($map, $seats) {
 
-        $counter = 0;
-        $reserve = [];            
+     //$reserve =array();  
+
 
     for($i=0; $i<count($map); $i++){
+    $counter = 0;    
     $row = $i + 1;
+    //$reserve = ["Ряд $row"];
+
+    //$search = (count($map[$i])-$seats)+ 1;
 
         for($j=0; $j<(count($map[$i])-$seats + 1); $j++) {
          $place = $j +1;
- 
+         //$reserve[] =  "Место $place";
+
+
             if($counter !== $seats) {
 
                 if($map[$i][$j] === FALSE) {
-                    $counter++;                 
-                    $reserve[] =  "ряд:  $row, место: $place"; 
+                    $counter++;
+                    $reserve[] = "ряд:  $row, место: $place"; 
 
                 } else {
-                   $counter = 0;
+                  
+                   $counter=0;
+                   $reserve =array();
+                   
                 }
 
 
             }else {
-                echo "$seats места забронированы:".PHP_EOL;
+
+            	
+                echo "$seats места забронированы:";
+
                 if (isset($reserve)) {
+
              foreach ($reserve as $key => $value) {
-               	echo $value.PHP_EOL;
+
+               	echo $value;
+
                 }
+
                }
-                return $reserve;
+
+               return $reserve;
+
             }
+
        }     
+
     }
+
 }
+
 groupReserve($map, 3);
 
 
 
 ?>
-</pre>
+
+
+
+
+ 
