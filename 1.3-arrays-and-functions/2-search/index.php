aef8c6e2b925814681e9d1db3751b24cb1aa773a
Anastasia <atamantseva2016@ya.ru>
2019-09-26 22:40:18 +0300

    places done

--- 1.3-arrays-and-functions/2-search/index.php
+++ 1.3-arrays-and-functions/2-search/index.php
@@ -19,65 +19,50 @@ $map = [
 
 ];
 
+
+
 function groupReserve($map, $seats) {
 
-     //$reserve =array();  
+    $counter = 0;
+    $reserve = array();
+
+    for ($i = 0; $i < count($map); $i++) {
+        $row = $i + 1;
+        $search = count($map[$i]) - $seats + 1;
+
+        for ($j = 0; $j <=$search; $j++) {
+             $place = $j +1;
+            if ($map[$i][$j] == FALSE) {
+                $counter++;
+                $reserve[] = "ряд: $row, место: $place; ";
+
+            } else {
+                $counter = 0;                
+                $reserve = array();
+
+            }
 
 
-    for($i=0; $i<count($map); $i++){
-    $counter = 0;    
-    $row = $i + 1;
-    $reserve = ["Ряд $row"];
-
-    //$search = (count($map[$i])-$seats)+ 1;
-
-        for($j=0; $j<(count($map[$i])-$seats + 1); $j++) {
-         $place = $j +1;
-         $reserve[] =  "Место $place";
-
-
-            if($counter !== $seats) {
-
-                if($map[$i][$j] === FALSE) {
-                    $counter++;
-                    //$reserve[] = "ряд:  $row, место: $place"; 
-
-                } else {
-                  
-                   $counter=0;
-                   $reserve =array();
-                   
-                }
-
-
-            }else {
-
-            	
+            if ($counter == $seats) {
                 echo "$seats места забронированы:";
-
                 if (isset($reserve)) {
-
-             foreach ($reserve as $key => $value) {
-
-               	echo $value;
+                foreach ($reserve as $key => $value) {
+              	echo $value;
 
                 }
 
                }
-
-               return $reserve;
+             
 
             }
 
-       }     
+        }
 
     }
 
 }
 
 groupReserve($map, 3);
-
-
 
 ?>
 
